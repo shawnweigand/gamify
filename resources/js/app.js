c716be6a81ed2cbb1eb766ac011c1bf13e9b241d
Shawn Weigand <shawnweigand1@gmail.com>
2022-08-11 23:14:58 -0400

    inertia and tailwind

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
-import './bootstrap';
+import React from 'react'
+import { render } from 'react-dom'
+import { createInertiaApp } from '@inertiajs/inertia-react'
+import { InertiaProgress } from '@inertiajs/progress'
+
+InertiaProgress.init()
+
+createInertiaApp({
+  resolve: name => require(`./Pages/${name}`),
+  setup({ el, App, props }) {
+    render(<App {...props} />, el)
+  },
+})
