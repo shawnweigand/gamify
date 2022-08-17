9d83099534b9ea193be4ced8305a95f2ad2856fb
Shawn Weigand <shawnweigand1@gmail.com>
2022-08-16 23:47:08 -0400

    middleware and typos

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -19,13 +19,13 @@ export default function Welcome(props) {
                             Your personal reward system to leveling up in the game of life
                         </div>
                         <div className='flex place-content-center pt-5'>
-                            <div class="flex m-2 space-x-2 justify-center">
-                                <button type="button" class="inline-block px-6 py-2.5 bg-green-400 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-green-700 hover:shadow-lg focus:bg-green-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-800 active:shadow-lg transition duration-150 ease-in-out">
+                            <div className="flex m-2 space-x-2 justify-center">
+                                <button type="button" className="inline-block px-6 py-2.5 bg-green-400 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-green-700 hover:shadow-lg focus:bg-green-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-800 active:shadow-lg transition duration-150 ease-in-out">
                                     <a href={route('login')}>Log in</a>
                                 </button>
                             </div>
-                            <div class="flex m-2 space-x-2 justify-center">
-                                <button type="button" class="inline-block px-6 py-2.5 bg-indigo-400 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-indigo-700 hover:shadow-lg focus:bg-indigo-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-indigo-800 active:shadow-lg transition duration-150 ease-in-out">
+                            <div className="flex m-2 space-x-2 justify-center">
+                                <button type="button" className="inline-block px-6 py-2.5 bg-indigo-400 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-indigo-700 hover:shadow-lg focus:bg-indigo-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-indigo-800 active:shadow-lg transition duration-150 ease-in-out">
                                     <a href={route('register')}>Register</a>
                                 </button>
                             </div>
