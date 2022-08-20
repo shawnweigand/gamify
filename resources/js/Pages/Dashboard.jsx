b40101fb462e99ac2e0c7686cf22f10e41976b6e
Shawn Weigand <shawnweigand1@gmail.com>
2022-08-19 20:42:12 -0400

    loop through quests

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -29,6 +29,11 @@ export default function Dashboard(props) {
                     <div className='mb-5 text-lg'>
                        ⚔️ Quests ⚔️
                     </div>
+
+                    {props.quests.map((quest) => (
+                        <div>{quest.name}</div>
+                    ))}
+                    
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 bg-white border-b border-gray-200">
                             <form onSubmit={submit}>
