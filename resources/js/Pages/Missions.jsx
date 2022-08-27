aecaa0834e5d4f90d4565ad163317e84cb3d3520
Shawn Weigand <shawnweigand1@gmail.com>
2022-08-27 16:37:31 -0400

    separate missions component

---
+++ resources/js/Pages/Missions.jsx
@@ -0,0 +1,16 @@
+import { Link } from '@inertiajs/inertia-react';
+import React from 'react';
+
+export default function Missions(props) {
+    
+    return (
+        <>
+        <div className='p-4 relative'>
+            {props.mission.name}
+            <Link href={route('missions.destroy',props.mission.id)} method="delete" preserveScroll="true" as="button" type="button" className="absolute right-0 top-3 inline-block mr-5 px-6 py-2.5 bg-red-400 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out">
+                Delete
+            </Link>
+        </div>
+        </>
+    )
+}
--- resources/js/Pages/Quests.jsx
+++ resources/js/Pages/Quests.jsx
@@ -1,3 +1,4 @@
+import Missions from '@/Pages/Missions';
 import { Link } from '@inertiajs/inertia-react';
 import React from 'react';
 
@@ -11,12 +12,7 @@ export default function Quests(props) {
                 response.push(mission);
             })
             return (response.map((mission) => (
-                <div className='p-4 relative'>
-                    {mission.name}
-                    <Link href={route('missions.destroy',mission.id)} method="delete" preserveScroll="true" as="button" type="button" className="absolute right-0 top-3 inline-block mr-5 px-6 py-2.5 bg-red-400 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out">
-                        Delete
-                    </Link>
-                </div>
+                <Missions mission={mission}/>
             )));
         }
         return "No missions";
